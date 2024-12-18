e5b0b72e5cf9cc0de39f74959cf945837b5e1003
Sreejita Dutta <sreejita.dutta@thoughtworks.com>
2024-12-18 12:06:16 +0530

    updating with checkout func

---
+++ vars/checkout.groovy
@@ -0,0 +1,8 @@
+def checkout (map details){
+     checkout( [
+            $class: 'GitSCM',
+            userRemoteConfigs: [[url: details.url]],
+            branches: [[name: details.branch]],
+         ])
+}
+    
