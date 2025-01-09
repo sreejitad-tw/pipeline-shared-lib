f87b12bdd8f8874a7669e632aa32622c408094e8
Sreejita Dutta <sreejita.dutta@thoughtworks.com>
2025-01-09 14:59:12 +0530

    resolving issue wioth scm

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,10 +1,10 @@
 def checkout_func (Map details){
-    def scmConfig = [
-        $class: 'GitSCM',
-        branches: [[name: "*/${details.branch}"]],
-        userRemoteConfigs: [[url: deatils.url]]
-    ]
-    checkout scm: scmConfig
+     checkout(
+        scmGit(
+            branches: [[name:  details.branch ]],
+            userRemoteConfigs: [[ url: details.url ]]
+        )
+    )
 
 }
     
