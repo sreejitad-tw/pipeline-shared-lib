af673cf852244f38ecb29e755af0e311e2e684a9
Sreejita Dutta <sreejita.dutta@thoughtworks.com>
2025-01-09 15:42:31 +0530

    resolving issue with scm 4

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -2,7 +2,7 @@ def checkout_func (Map details){
     checkout(
         scmGit(
             branches: [[name:  details.branch ]],
-            userRemoteConfigs: [[ url: details.url ]]
+            remote: [[ url: details.url ]]
         )
     )
 
